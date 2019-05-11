698d06b2ae2068e4e21282e40d00f5f3feb69d69
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2019-05-11 03:57:13 -0500

    Remove Draw2PNG and add index.html redirector.

--- draw2png/res/js/Draw2PNG/src/functions/extend.js
+++
@@ -1,7 +0,0 @@
-/**
- * Extends child class from parent class.
- */
-function extend (child, parent) {
-  child.prototype             = (typeof Object['create'] == 'function') ? Object.create(parent.prototype) : new parent ;
-  child.prototype.constructor = child;
-}
--- draw2png/res/js/Draw2PNG/src/functions/removeFileNameExtension.js
+++
@@ -1,8 +0,0 @@
-/**
- * Returns the specified file name without its extension. Ex. filename.txt -> filename
- */
-function removeFileNameExtension (fileName) {
-  var parts = fileName.split('.');
-  
-  return (parts.length > 1) ? parts.splice(0, parts.length - 1).join('.') : fileName ;
-}
