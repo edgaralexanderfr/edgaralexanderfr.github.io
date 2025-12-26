825f691ae065f1e92a6fac9a608ba97044087ccd
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2025-12-26 13:22:16 -0500

    Basic initial config for the project.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    compressHTML: false, // Leaving it at `true` at the moment for development purposes...
+    server: {
+        port: 8080,
+    },
+});
