8eebd3e38f879ab6abf270113572a5e615873616
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2025-12-26 14:26:17 -0500

    Create basic organization for common page template.

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,9 @@ export default defineConfig({
 
   server: {
       port: 8080,
+      allowedHosts: [
+        "dev.edgaralexanderfr.com",
+      ],
   },
 
   integrations: [svelte()],
---
+++ src/components/page/Head.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import type { Snippet } from "svelte";
+
+    type Props = {
+        title: string;
+        children: Snippet;
+    };
+
+    const { title, children }: Props = $props();
+</script>
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <meta name="viewport" content="width=device-width" />
+    <title>{title}</title>
+
+    {@render children()}
+</head>
