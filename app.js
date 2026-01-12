306065ce0e28b3d79b5afa1e5a8694b6743c7789
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2026-01-12 12:01:25 -0500

    Add Software Mind Project/Entry to Portfolio.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const serveStatic = require("serve-static");
 const path = require('path');
 
-const port = 9999;
+const port = 8080;
 
 app = express();
 app.use(serveStatic(path.join(__dirname, './')));
