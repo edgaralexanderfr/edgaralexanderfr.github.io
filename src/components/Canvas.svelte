0d6527d6148a5ff6fd2569cfa4733324223ac97d
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2026-01-17 19:29:23 -0500

    First attempt to load Scene through Threlte component.

---
+++ src/components/Canvas.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { T, Canvas } from '@threlte/core';
+	import Model from './Model.svelte';
+</script>
+
+<Canvas>
+	<T.Color attach="background" args={['#4ED7F1']} />
+
+	<Model />
+</Canvas>
