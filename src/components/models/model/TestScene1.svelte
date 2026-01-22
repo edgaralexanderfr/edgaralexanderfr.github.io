e4273eb1c634507aaeb4c7c86cb0ddb4009d1fd7
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2026-01-22 17:26:18 -0500

    Create test-scene-1 and test-scene-2 pages.

---
+++ src/components/models/model/TestScene1.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { T, Canvas } from '@threlte/core';
+	import Model from '../../../models/model/TestScene1.svelte';
+	import { ACESFilmicToneMapping, LoopOnce } from 'three';
+
+	let scene = $state<Model>();
+
+	const actions = $derived(scene?.actions);
+	const walkAction = $derived($actions?.['Walk']);
+	const cameraAction = $derived($actions?.['CameraAction']);
+	const doorAction = $derived($actions?.['DoorAction']);
+
+	$effect(() => {
+		if (walkAction) {
+			walkAction.clampWhenFinished = true;
+		}
+
+		if (cameraAction) {
+			cameraAction.clampWhenFinished = true;
+		}
+
+		if (doorAction) {
+			doorAction.clampWhenFinished = true;
+		}
+
+		walkAction?.setLoop(LoopOnce, 1);
+		cameraAction?.setLoop(LoopOnce, 1);
+		doorAction?.setLoop(LoopOnce, 1);
+
+		walkAction?.play();
+		cameraAction?.play();
+		doorAction?.play();
+	});
+</script>
+
+<Canvas toneMapping={ACESFilmicToneMapping} shadows={false}>
+	<T.Color attach="background" args={['#4ED7F1']} />
+
+	<Model bind:this={scene} />
+</Canvas>
---
+++ src/components/models/model/TestScene2.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { T, Canvas } from '@threlte/core';
+	import Model from '../../../models/model/TestScene2.svelte';
+	import { ACESFilmicToneMapping, LoopOnce } from 'three';
+
+	let scene = $state<Model>();
+
+	const actions = $derived(scene?.actions);
+	const walkAction = $derived($actions?.['Edgar.Armature']);
+	const cameraAction = $derived($actions?.['Camera']);
+	const doorAction = $derived($actions?.['Door']);
+	const lidAction = $derived($actions?.['Lid']);
+
+	$effect(() => {
+		if (walkAction) {
+			walkAction.clampWhenFinished = true;
+		}
+
+		if (cameraAction) {
+			cameraAction.clampWhenFinished = true;
+		}
+
+		if (doorAction) {
+			doorAction.clampWhenFinished = true;
+		}
+
+		if (lidAction) {
+			lidAction.clampWhenFinished = true;
+		}
+
+		walkAction?.setLoop(LoopOnce, 1);
+		cameraAction?.setLoop(LoopOnce, 1);
+		doorAction?.setLoop(LoopOnce, 1);
+		lidAction?.setLoop(LoopOnce, 1);
+
+		walkAction?.play();
+		cameraAction?.play();
+		doorAction?.play();
+		lidAction?.play();
+	});
+</script>
+
+<Canvas toneMapping={ACESFilmicToneMapping} shadows={false}>
+	<T.Color attach="background" args={['#4ED7F1']} />
+
+	<Model bind:this={scene} />
+</Canvas>
